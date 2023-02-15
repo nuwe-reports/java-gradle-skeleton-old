c7e3b65ee8344a1834e7dd216af1e2123dbe8c3f
Andreu Vall Hernàndez <andreu.vallhernandez@gmail.com>
2023-02-15 12:52:32 +0100

    Simplified

---
+++ src/main/java/First.java
@@ -0,0 +1,6 @@
+public class First {
+
+    public static int sum2(int a, int b) {
+        return a + b;
+    }
+}
---
+++ src/test/java/FirstTest.java
@@ -0,0 +1,16 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class FirstTest {
+
+    @Test
+    void myFirstTest() {
+        assertEquals(First.sum2(3, 4), 7);
+    }
+
+    /*@Test
+    void myFailedTest() {
+        assertEquals(First.sum2(3, 9), 9);
+    }*/
+}
